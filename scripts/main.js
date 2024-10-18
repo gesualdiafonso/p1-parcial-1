ad15400655d4512eb851c9fec95f337bea744fb5
Afonso Gesualdi <gesualdiafonsoarr@outlook.com>
2024-10-17 21:20:04 -0300

    "aaa"

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,7 @@ function cargarDiscos() {
         codigoUsado.push(idDisco);
 
         // Creamos una nueva instancia del disco con los datos validados
-        const nuevoDisco = new Disco(nombreDisco, nombreArtista, idDisco);
+        const nuevoDisco = new Disco(nombreDisco, nombreArtista, idDisco, '');
 
         //Seguimos con la carga de información para que sea fornecido las pistas adentro del bucle
         let seguir; //Declaro seguir para que pueda ser seguido la carga de datos
@@ -37,17 +37,21 @@ function cargarDiscos() {
 
             seguir = confirm("Deseas seguir agregando mas canciones?")
 
-        }while(seguir)
+        }while (seguir)
 
         discos.push(nuevoDisco);
 
         alert(`El disco ${nombreDisco} de ${nombreArtista} con el Id: ${idDisco}, ha sido cargado con éxito!`)
-    }while(confirm("Deseas cargar mas discografia?"))
+    }while (confirm("Deseas cargar mas discografia?"))
 }
 function mostrarDisco(){
 
     const contenedor = document.getElementById('container')
     const biblioteca = new Biblioteca();
+
+    for(let disco of discos){
+        biblioteca.discos.push(disco)
+    }
 
     setTimeout(() => {
         contenedor.innerHTML = biblioteca.toHTML();
